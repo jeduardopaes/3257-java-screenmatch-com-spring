066783e4c99903561698974db3bc90c73de1b959
Eduardo Paes <j.eduardopaes@gmail.com>
2024-08-28 05:58:28 -0400

    Getting a List for all the seasons of a serie and printing

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,46 +1,58 @@
 package br.com.alura.screenmatch;
 
-import br.com.alura.screenmatch.model.DadosEpisodio;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.model.DadosTemporada;
 import br.com.alura.screenmatch.service.ConsumoApi;
 import br.com.alura.screenmatch.service.ConverteDados;
 import io.github.cdimascio.dotenv.Dotenv;
 
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ScreenmatchApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ScreenmatchApplication.class, args);
+    }
 
-	@Override
-	public void run(String... args) throws Exception {
+    @Override
+    public void run(String... args) throws Exception {
 
-		// Load .env Variables
-		Dotenv dotenv = Dotenv.load();
-		// System.out.println(String.format("APIKey IS: %s %n%n%n",
-		// dotenv.get("APIkey")));
+        // Load .env Variables
+        Dotenv dotenv = Dotenv.load();
+        // System.out.println(String.format("APIKey IS: %s %n%n%n",
+        // dotenv.get("APIkey")));
 
-		var consumoApi = new ConsumoApi();
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=supernatural&apikey=" + dotenv.get("APIkey"));
-		// System.out.println(json);
-		// json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
-		// System.out.println(json);
-		ConverteDados conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(String.format("%n%n%s%n%n", dados));
+        var consumoApi = new ConsumoApi();
+        var json = consumoApi.obterDados("https://www.omdbapi.com/?t=supernatural&apikey=" + dotenv.get("APIkey"));
+        // System.out.println(json);
+        // json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
+        // System.out.println(json);
+        ConverteDados conversor = new ConverteDados();
+        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
 
-		DadosTemporada temporada = conversor.obterDados(
-				consumoApi
-						.obterDados("https://www.omdbapi.com/?t=supernatural&season=1&apikey=" + dotenv.get("APIkey")),
-						DadosTemporada.class);
+        System.out.println("\n\n" + dados + "\n\n");
 
-		System.out.println(String.format("%n%n%s%n%n", temporada));
+        List<DadosTemporada> temporadas = new ArrayList<>();
 
-	}
+        for (int i = 1; i <= dados.totalTemporadas(); i++) {
+            DadosTemporada temporada = conversor.obterDados(
+                    consumoApi
+                            .obterDados("https://www.omdbapi.com/?t=supernatural&season=" + i + "&apikey=" + dotenv.get("APIkey")),
+                    DadosTemporada.class);
+
+            temporadas.add(temporada);
+
+        }
+
+        temporadas.forEach(season -> {
+            System.out.println("\n" + season);
+        });
+
+    }
 }
