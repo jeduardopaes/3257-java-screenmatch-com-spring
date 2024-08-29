9de1a399157afcabb3d9c5bb118caffb4191d9fe
Eduardo Paes <j.eduardopaes@gmail.com>
2024-08-29 04:40:33 -0400

    Created a principal to deal with prints on console and search by serie name

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,17 +1,10 @@
 package br.com.alura.screenmatch;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import br.com.alura.screenmatch.model.DadosSerie;
-import br.com.alura.screenmatch.model.DadosTemporada;
-import br.com.alura.screenmatch.service.ConsumoApi;
-import br.com.alura.screenmatch.service.ConverteDados;
-import io.github.cdimascio.dotenv.Dotenv;
+import br.com.alura.screenmatch.principal.Principal;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -23,36 +16,8 @@ public class ScreenmatchApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        // Load .env Variables
-        Dotenv dotenv = Dotenv.load();
-        // System.out.println(String.format("APIKey IS: %s %n%n%n",
-        // dotenv.get("APIkey")));
+        Principal principal = new Principal();
 
-        var consumoApi = new ConsumoApi();
-        var json = consumoApi.obterDados("https://www.omdbapi.com/?t=supernatural&apikey=" + dotenv.get("APIkey"));
-        // System.out.println(json);
-        // json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
-        // System.out.println(json);
-        ConverteDados conversor = new ConverteDados();
-        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-
-        System.out.println("\n\n" + dados + "\n\n");
-
-        List<DadosTemporada> temporadas = new ArrayList<>();
-
-        for (int i = 1; i <= dados.totalTemporadas(); i++) {
-            DadosTemporada temporada = conversor.obterDados(
-                    consumoApi
-                            .obterDados("https://www.omdbapi.com/?t=supernatural&season=" + i + "&apikey=" + dotenv.get("APIkey")),
-                    DadosTemporada.class);
-
-            temporadas.add(temporada);
-
-        }
-
-        temporadas.forEach(season -> {
-            System.out.println("\n" + season);
-        });
-
+        principal.displayMenu();
     }
 }
---
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -0,0 +1,71 @@
+package br.com.alura.screenmatch.principal;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.model.DadosTemporada;
+import br.com.alura.screenmatch.service.ConsumoApi;
+import br.com.alura.screenmatch.service.ConverteDados;
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class Principal {
+
+    Dotenv dotenv = Dotenv.load();
+    private ConsumoApi consumoApi = new ConsumoApi();
+    private ConverteDados conversor = new ConverteDados();
+    private Scanner scan = new Scanner(System.in);
+    private final String ENDERECO = "https://www.omdbapi.com/?t=";
+    private final String API_KEY = "&apikey=" + dotenv.get("APIkey");
+    private final String SEASONS_PARAM = "&season=";
+
+    public void displayMenu() {
+        System.out.println("Digite o nome da serie que deseja buscar: ");
+        var nomeSerie = scan.nextLine();
+
+        var enderecoAPI = ENDERECO + nomeSerie.replace(" ", "+") + API_KEY;
+
+        var json = consumoApi.obterDados(enderecoAPI);
+
+        DadosSerie serie = conversor.obterDados(json, DadosSerie.class);
+
+        clearScreen();
+
+        System.out.println("========= Serie: " + serie.titulo() + " =========\nNumero de Temporadas: " + serie.totalTemporadas() + "\n"
+                + "Score: " + serie.avaliacao() + " \n==================\n\n\n");
+
+        displaySeasons(serie);
+
+    }
+
+    public void displaySeasons(DadosSerie serie) {
+
+        List<DadosTemporada> temporadas = new ArrayList<>();
+
+        for (int i = 1; i <= serie.totalTemporadas(); i++) {
+            DadosTemporada temporada = conversor.obterDados(
+                    consumoApi
+                            .obterDados(ENDERECO + serie.titulo().replace(" ", "+") + API_KEY + SEASONS_PARAM + i),
+                    DadosTemporada.class);
+
+            temporadas.add(temporada);
+
+        }
+
+        temporadas.forEach(season -> {
+            System.out.println("================= Season: " + season.numero() + " =================");
+            season.episodios().forEach(episodio -> {
+                System.out.println(episodio.numeroEpisodio() + "- Episode Title: " + episodio.titulo());
+            });
+            System.out.println("======================================");
+        });
+    }
+
+    private void clearScreen() {
+        for (int i = 0; i < 50; i++) {
+            System.out.println();
+        }
+    }
+
+}
